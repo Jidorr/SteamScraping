6a125373a9d2bcc57622ec22f1f3f1da4feaadf4
Jordi Salleras <jordisa97@uoc.edu>
2022-10-27 15:04:23 +0200

    Noms i preus dels jocs de la primera pagina

--- src/steamScraper.py
+++ src/steamScraper.py
@@ -7,4 +7,10 @@ url = 'https://store.steampowered.com/search/results/?query&start=0&count=50&dyn
 
 page = requests.get(url)
 data = page.json()
-print(data['results_html'])
+soup = BeautifulSoup(data['results_html'], "html.parser")
+all_games = soup.find_all('a')
+
+for game in all_games:
+    print('******************************')
+    print(game.find('span', {'class': 'title'}).text.strip())
+    print(game.find('div', {'class': 'col search_price_discount_combined responsive_secondrow'}).text.strip())
